5ec2840148c717043ca6d54b219dee940bbbf81f
fengxiaojie <1106963189@qq.com>
2023-11-14 14:59:47 +0800

    Update main.go

--- main.go
+++ main.go
@@ -5,11 +5,11 @@ import (
 	"os/signal"
 	"syscall"
 
-	C "github.com/Dreamacro/clash/constant"
-	"github.com/Dreamacro/clash/hub"
-	"github.com/Dreamacro/clash/proxy/http"
-	"github.com/Dreamacro/clash/proxy/socks"
-	"github.com/Dreamacro/clash/tunnel"
+	C "github.com/fengxiaojie/clash-backup/constant"
+	"github.com/fengxiaojie/clash-backup/hub"
+	"github.com/fengxiaojie/clash-backup/proxy/http"
+	"github.com/fengxiaojie/clash-backup/proxy/socks"
+	"github.com/fengxiaojie/clash-backup/tunnel"
 
 	log "github.com/sirupsen/logrus"
 )
